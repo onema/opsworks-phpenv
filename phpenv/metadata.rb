66e2e4bc4e5357b0ec9290405ea5627a5bcabf55
Juan Manuel Torres <kinojman@gmail.com>
2015-06-12 23:10:06 -0700

    Update license

--- phpenv/metadata.rb
+++ phpenv/metadata.rb
@@ -1,7 +1,7 @@
 name              "phpenv"
 maintainer        "Juan Manuel Torres"
 maintainer_email  "kinojman@gmail.com"
-license           "MIT"
+license           "Apache 2"
 description       "Set of recipes to help setup PHP applications ."
 version           "1.0.0"
 
